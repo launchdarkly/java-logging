db02251462dd58f8c278d9ff474957e5fd42694e
Eli Bishop <eli@launchdarkly.com>
2022-01-11 17:45:01 -0800

    use java-test-helpers

--- src/test/java/com/launchdarkly/logging/LogCaptureMessageTest.java
+++ src/test/java/com/launchdarkly/logging/LogCaptureMessageTest.java
@@ -1,9 +1,10 @@
 package com.launchdarkly.logging;
 
+import com.launchdarkly.testhelpers.TypeBehavior;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,18 +22,15 @@ public class LogCaptureMessageTest extends BaseTest {
   
   @Test
   public void equalInstancesAreEqual() {
-    List<List<LogCapture.Message>> testValues = new ArrayList<>();
+    List<TypeBehavior.ValueFactory<LogCapture.Message>> valueFactories = new ArrayList<>();
     for (String name: new String[] { "name1", "name2" }) {
       for (LDLogLevel level: LDLogLevel.values()) {
         for (String text: new String[] { "text1", "text2" }) {
-          testValues.add(Arrays.asList(
-              new LogCapture.Message(name, level, text),
-              new LogCapture.Message(name, level, text)
-              ));
+          valueFactories.add(() -> new LogCapture.Message(name, level, text));
         }
       }
     }
-    TestHelpers.doEqualityTests(testValues);
+    TypeBehavior.checkEqualsAndHashCode(valueFactories);
   }
   
   @Test
